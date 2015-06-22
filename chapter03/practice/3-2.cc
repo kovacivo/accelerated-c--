262dd80dacf9645761b1129df7317d767a439fb2
ivo kovac <ivo@debian>
2015-06-22 10:42:02 +0200

    exercise 3-2 seems to be finished

--- chapter03/practice/3-2.cc
+++ chapter03/practice/3-2.cc
@@ -16,7 +16,7 @@ using std::setprecision;    using std::vector;
 
 int main()
 {
-  cout << "This program computes and prints the quartiles of a set of integers (that is, the quarter of the numbers with the largest values, the next highest quarter, and so on)";
+  cout << "This program computes and prints the quartiles of a set of integers from the largest quarter up to the lowest quarter)" << endl;
 
 	// ask for and read the numbers 
 	cout << "Enter all your integers, followed by end-of-file (CTRL+D): ";
@@ -26,8 +26,8 @@ int main()
 	while (cin >> x)
 		integers.push_back(x);
 
-	// check that the student entered some homework grades
-	typedef vector<double>::size_type vec_sz;
+	// check that the user entered some numbers
+	typedef vector<int>::size_type vec_sz;
 	vec_sz integers_size = integers.size();
 	if (integers_size == 0) {
 		cout << endl << "You must enter your numbers.  "
@@ -38,11 +38,21 @@ int main()
 	// sort the integers
 	sort(integers.begin(), integers.end());
 
+  int quartile_size = integers.size()/4;
+  cout << "Quartile size is " << quartile_size << endl;
+
+  int tmp = 0;
   while(integers.size() > 0) {
-    cout << integers[(integers.size()-1)] << " ";
-  	integers.pop_back(); 
+    tmp = 0;
+    while (tmp < quartile_size) {
+    	cout << integers[(integers.size()-1)] << " ";
+  	  integers.pop_back(); 
+      tmp++;
+    }
+    cout << endl;
   }
 
+  cout << "Program now ends." << endl;
 	return 0;
 }
 
